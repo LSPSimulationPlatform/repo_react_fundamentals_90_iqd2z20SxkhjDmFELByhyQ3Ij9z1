19e26f9ee9cb8f47bfdc544c0e27731a9d5a8d39
maqaexx <ashurovmahammad1@gmail.com>
2025-08-20 19:51:23 +0000

    functionalities where added to app.js and productcreate.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import ProductCreate from "./components/ProductCreate";
 
 function App() {
-  return (
-   <ProductCreate/>
-  )
+  const [products, setProducts] = useState([]);
+
+  const addProduct = (newProduct) => {
+    console.log("New Product", newProduct);
+    setProducts((prev) => [...prev, newProduct]);
+  };
+  return (<div><ProductCreate onAddProduct={addProduct} /></div>);
 }
 
-
-
-export default App;
+export default App; // Export the App component as the default export
